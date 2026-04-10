e5d37e2cf7691a9aa8627e4ac0962c2a89aee86b
kelmaitisjoanas <kelmaitisjoanas@gmail.com>
2026-04-10 20:59:11 +0200

    Update main.tsx

--- vision/src/main.tsx
+++ vision/src/main.tsx
@@ -1,5 +1,3 @@
-// Provide a complete list of UUIDs assosiated with the **Attacker** to /home/kali/.uuids
-# Provide a complete list of UUIDs assosiated with the **Attacker** to /home/kali/.uuids
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
